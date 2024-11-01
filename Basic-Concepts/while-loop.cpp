20db7a601399fc4871b0c24c1daf44f60dafc467
mayur4279 <mayurdhotre.a15@gmail.com>
2024-11-01 20:54:38 +0530

    while loop code is added

---
+++ Basic-Concepts/while-loop.cpp
@@ -0,0 +1,18 @@
+// printing { 1-10 } Numbers  
+
+#include <iostream>   
+using namespace std;   
+
+int  main ()   {
+    int a = 0  ;  
+
+    while (a <= 10 ) {
+       
+    cout  <<  a << endl ;   
+    a++ ;  
+ }  
+
+    return 0;   
+}
+
+
